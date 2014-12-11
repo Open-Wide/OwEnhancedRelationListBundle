9c44ea5100e4fa6cdec6902d91fbcd7cf0bf89a1
Fabien Jarnet <fabien.jarnet@openwide.fr>
2014-12-11 10:26:21 +0100

    return relations available in the current language only

--- Helper/CommonHelper.php
+++ Helper/CommonHelper.php
@@ -3,11 +3,12 @@
 namespace Ow\Bundle\OwEnhancedRelationListBundle\Helper;
 
 use eZ\Publish\API\Repository\Repository;
+use Symfony\Component\DependencyInjection\ContainerAware;
 
 /**
  * Helper class for building many things :-)
  */
-class CommonHelper {
+class CommonHelper extends ContainerAware {
 
     /**
      * @var \eZ\Publish\API\Repository\Repository
@@ -19,22 +20,38 @@ class CommonHelper {
     }
 
     /**
-     * @param $content \eZ\Publish\API\Repository\Values\Content
+     * @param $content \eZ\Publish\Core\Repository\Values\Content\Content
      * @param $attributeName string content attribute name
+     * @param $localeEz string language
      * @return owEnhancedRelationList object
      */
-    public function getFieldValue( $content, $attributeName ) {
+    public function getFieldValue( $content, $attributeName, $localeEz = '' ) {
         $locationService = $this->repository->getLocationService();
-        $owEnhancedRelationListFieldValue = $content->getFieldValue($attributeName);
+        $contentService = $this->repository->getContentService();
 
-        foreach ($owEnhancedRelationListFieldValue->destinationLocationIds as $k => $locationId) {
-            $location = $locationService->loadLocation($locationId);
-            if ($location->hidden || $location->invisible) {
-                // Entry not visible => don't return this location !
-                unset($owEnhancedRelationListFieldValue->destinationContentIds[$k]);
-                unset($owEnhancedRelationListFieldValue->destinationLocationIds[$k]);
+        if (!$localeEz) {
+            $localeConverter = $this->container->get('ezpublish.locale.converter');
+            $localeEz = $localeConverter->convertToEz($this->container->get('request')->getLocale());
+        }
+
+        $owEnhancedRelationListFieldValue = $content->getFieldValue($attributeName, $localeEz);
+
+        if ($owEnhancedRelationListFieldValue) {
+            foreach ($owEnhancedRelationListFieldValue->destinationLocationIds as $k => $locationId) {
+                $relationLocation = $locationService->loadLocation($locationId);
+                $relationContent = $contentService->loadContentByContentInfo($relationLocation->getContentInfo());
+
+                if ($relationLocation->hidden
+                     || $relationLocation->invisible
+                     || (!in_array($localeEz, $relationContent->getVersionInfo()->languageCodes) && !$relationLocation->getContentInfo()->alwaysAvailable)
+                ) {
+                    // Entry not available => don't return this
+                    unset($owEnhancedRelationListFieldValue->destinationContentIds[$k]);
+                    unset($owEnhancedRelationListFieldValue->destinationLocationIds[$k]);
+                }
             }
         }
+
         return $owEnhancedRelationListFieldValue;
     }
 
